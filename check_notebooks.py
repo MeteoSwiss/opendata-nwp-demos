486d7f7727d509061695237fd81b2fea1b13245d
nnellen <193914323+nnellen@users.noreply.github.com>
2026-09-04 12:31:33 +0200

    update try, except and typos

--- check_notebooks.py
+++ check_notebooks.py
@@ -28,7 +28,7 @@ def run_notebook(notebook_path: Path):
 
 def main():
     had_errors = False
-    failed_notebookes = []
+    failed_notebooks = []
 
     for directory in NOTEBOOK_DIRS:
         for nb_path in directory.glob("*.ipynb"):
@@ -36,11 +36,11 @@ def main():
                 run_notebook(nb_path)
             except Exception:
                 had_errors = True
-                failed_notebookes.append(nb_path)
+                failed_notebooks.append(nb_path)
 
     if had_errors:
-        failed_notebooks = "\n".join(f"  - {nb_path}" for nb_path in failed_notebooks)
-        logger.error(f"The following notebooks failed:\n{failed_notebooks}")
+        failed_notebooks_str = "\n".join(f"  - {nb_path}" for nb_path in failed_notebooks)
+        logger.error(f"The following notebooks failed:\n{failed_notebooks_str}")
         sys.exit(1)
     else:
         logger.info("All notebooks ran successfully.")
@@ -49,3 +49,4 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
     main()
+
